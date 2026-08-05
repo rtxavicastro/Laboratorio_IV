76dc2d84ea1ef52b534717bd260acd1899c36e24
Xavier Quina <rtxavicastro11@gmail.com>
2026-08-04 23:33:16 -0600

    Ejercicio 5 terminado

---
+++ Ejercicio 5/validacionEdad.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class validacionEdad {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int edad = 0;
+        System.out.print("Ingrese su edad: ");
+        edad = entrada.nextInt();
+        try{
+            if(edad < 0){
+                throw new IllegalArgumentException("Error: La edad no puede ser negativa");
+            }
+            System.out.println("Tienes "+edad+" años");
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
